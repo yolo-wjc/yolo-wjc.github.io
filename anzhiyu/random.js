69f949f3ec4b1b7f85b05928c9153faedfca487e
yolo-wjc <772993121@qq.com>
2025-02-01 18:10:37 +0800

    Site updated: 2025-02-01 18:10:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/05/hello-world/"];function toRandomPost(){
+var posts=["2025/02/01/Hello World！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
